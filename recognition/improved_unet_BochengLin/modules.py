128ec216848c1d609b8db571217b9beadf276134
BOCHENG LIN (TRIWOODBC) <triwoodbc@gmail.com>
2025-10-20 22:08:57 +1000

    fix: Fix UpBlockImproved channel dimensions

--- recognition/improved_unet_BochengLin/modules.py
+++ recognition/improved_unet_BochengLin/modules.py
@@ -38,7 +38,8 @@ class UpBlockImproved(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.up = nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2)
-        self.conv = ConvBlock(in_channels, out_channels)
+        # After concatenation with skip connection, channels are doubled
+        self.conv = ConvBlock(out_channels * 2, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
